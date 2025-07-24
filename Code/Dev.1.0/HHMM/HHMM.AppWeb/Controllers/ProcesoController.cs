6234ada412d00f7c753a5bfd43d46a46de98512a
RayReyes <ray.reyes@hte.com.pe>
2025-07-24 08:42:04 -0500

    Se modifica Ruta

--- Code/Dev.1.0/HHMM/HHMM.AppWeb/Controllers/ProcesoController.cs
+++ Code/Dev.1.0/HHMM/HHMM.AppWeb/Controllers/ProcesoController.cs
@@ -2033,6 +2033,10 @@ namespace HHMM.AppWeb.Controllers
                     string nombreArchivoSeguro = Path.GetInvalidFileNameChars()
 .Aggregate(nombre, (current, c) => current.Replace(c.ToString(), "_"));
 
+                    if (nombreArchivoSeguro.Contains("..") || nombreArchivoSeguro.Contains("/") || nombreArchivoSeguro.Contains("\\"))
+                    {
+                        return File(new byte[0], "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                    }
                     // 4. Configuración de rutas seguras
                     string carpeta = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "Files");
                     sArchivoXlsx = Path.Combine(carpeta, nombreArchivoSeguro);
